1824126092e63c5a0dbcb785b10f8df43ae0cea6
devthiagoh <devthiagoh@gmail.com>
2023-07-10 21:26:04 -0300

    Implementação de teste técnico - NetDeal - complementações

---
+++ src/main/java/br/com/netdeal/dto/ComplexityAndScoreDTO.java
@@ -0,0 +1,23 @@
+package br.com.netdeal.dto;
+
+public class ComplexityAndScoreDTO {
+
+	private String complexity;
+	private String score;
+	
+	public String getComplexity() {
+		return complexity;
+	}
+
+	public void setComplexity(String complexity) {
+		this.complexity = complexity;
+	}
+
+	public String getScore() {
+		return score;
+	}
+
+	public void setScore(String score) {
+		this.score = score;
+	}	
+}
